c2d0e43e9cd78d627999b15c6308a03369ccaa6f
Ben Woodward <ben@mercarto.com>
2020-05-24 21:36:47 +0100

    feat: add apex log

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gitlab.com/ben178/go-starter
 
 go 1.14
+
+require github.com/apex/log v1.1.4
--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"log"
+	"github.com/apex/log"
 )
 
 func main() {
-	log.Println("Hello World!")
+	log.Info("Hello World!")
 }
